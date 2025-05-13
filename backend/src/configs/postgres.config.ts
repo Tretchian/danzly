8903b4cc379509699c8ee0dd88f450dc98c7205e
Tretchian <safonovila7@gmail.com>
2025-05-14 01:14:35 +0300

    backend stabilazed!

--- backend/src/configs/postgres.config.ts
+++ backend/src/configs/postgres.config.ts
@@ -6,12 +6,12 @@ export const getPostgresConfig = async (
 ): Promise<TypeOrmModuleOptions> => {
   return {
     type: 'postgres',
-    host: configService.get('DATABASE_HOST'),
-    port: configService.get('DATABASE_PORT'),
-    username: configService.get('DATABASE_USERNAME'),
-    password: configService.get('DATABASE_PASSWORD'),
-    database: configService.get('DATABASE_NAME'),
-    synchronize: configService.get('DATABASE_SYNCHRONIZE') === 'true',
+    host: configService.get('DB_HOST'),
+    port: configService.get('DB_PORT'),
+    username: configService.get('DB_USERNAME'),
+    password: configService.get('DB_PASSWORD'),
+    database: configService.get('DB_NAME'),
+    synchronize: configService.get('DB_SYNCHRONIZE') === 'true',
     autoLoadEntities: true,
     logging: true,
   };
